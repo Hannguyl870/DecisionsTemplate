b1a34cffb07949048ce3dedcb31f23275da97afa
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-10-04 11:27:29 -0400

    code

--- Decisions/Form1.Designer.cs
+++ Decisions/Form1.Designer.cs
@@ -48,14 +48,19 @@ namespace Decisions
             this.yearOutput = new System.Windows.Forms.Label();
             this.yearInput = new System.Windows.Forms.TextBox();
             this.yearLabel = new System.Windows.Forms.Label();
+            this.integerlable = new System.Windows.Forms.Label();
+            this.integerinput = new System.Windows.Forms.TextBox();
+            this.Entebutton = new System.Windows.Forms.Button();
+            this.integeroutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // voteButton
             // 
             this.voteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.voteButton.Location = new System.Drawing.Point(310, 37);
+            this.voteButton.Location = new System.Drawing.Point(413, 46);
+            this.voteButton.Margin = new System.Windows.Forms.Padding(4);
             this.voteButton.Name = "voteButton";
-            this.voteButton.Size = new System.Drawing.Size(88, 29);
+            this.voteButton.Size = new System.Drawing.Size(117, 36);
             this.voteButton.TabIndex = 2;
             this.voteButton.Text = "VOTE";
             this.voteButton.UseVisualStyleBackColor = true;
@@ -65,9 +70,10 @@ namespace Decisions
             // 
             this.voteOutput.BackColor = System.Drawing.Color.White;
             this.voteOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.voteOutput.Location = new System.Drawing.Point(413, 37);
+            this.voteOutput.Location = new System.Drawing.Point(551, 46);
+            this.voteOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.voteOutput.Name = "voteOutput";
-            this.voteOutput.Size = new System.Drawing.Size(388, 29);
+            this.voteOutput.Size = new System.Drawing.Size(517, 36);
             this.voteOutput.TabIndex = 3;
             this.voteOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -75,26 +81,29 @@ namespace Decisions
             // 
             this.ageLabel.AutoSize = true;
             this.ageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ageLabel.Location = new System.Drawing.Point(80, 40);
+            this.ageLabel.Location = new System.Drawing.Point(107, 49);
+            this.ageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ageLabel.Name = "ageLabel";
-            this.ageLabel.Size = new System.Drawing.Size(145, 24);
+            this.ageLabel.Size = new System.Drawing.Size(182, 29);
             this.ageLabel.TabIndex = 4;
             this.ageLabel.Text = "Enter Your Age:";
             // 
             // ageInput
             // 
             this.ageInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ageInput.Location = new System.Drawing.Point(231, 37);
+            this.ageInput.Location = new System.Drawing.Point(308, 46);
+            this.ageInput.Margin = new System.Windows.Forms.Padding(4);
             this.ageInput.Name = "ageInput";
-            this.ageInput.Size = new System.Drawing.Size(60, 29);
+            this.ageInput.Size = new System.Drawing.Size(79, 34);
             this.ageInput.TabIndex = 5;
             // 
             // workButton
             // 
             this.workButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.workButton.Location = new System.Drawing.Point(310, 101);
+            this.workButton.Location = new System.Drawing.Point(413, 124);
+            this.workButton.Margin = new System.Windows.Forms.Padding(4);
             this.workButton.Name = "workButton";
-            this.workButton.Size = new System.Drawing.Size(88, 29);
+            this.workButton.Size = new System.Drawing.Size(117, 36);
             this.workButton.TabIndex = 6;
             this.workButton.Text = "WORK";
             this.workButton.UseVisualStyleBackColor = true;
@@ -104,9 +113,10 @@ namespace Decisions
             // 
             this.workOutput.BackColor = System.Drawing.Color.White;
             this.workOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.workOutput.Location = new System.Drawing.Point(413, 101);
+            this.workOutput.Location = new System.Drawing.Point(551, 124);
+            this.workOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.workOutput.Name = "workOutput";
-            this.workOutput.Size = new System.Drawing.Size(388, 29);
+            this.workOutput.Size = new System.Drawing.Size(517, 36);
             this.workOutput.TabIndex = 7;
             this.workOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -114,62 +124,69 @@ namespace Decisions
             // 
             this.hoursLabel.AutoSize = true;
             this.hoursLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hoursLabel.Location = new System.Drawing.Point(38, 101);
+            this.hoursLabel.Location = new System.Drawing.Point(51, 124);
+            this.hoursLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.hoursLabel.Name = "hoursLabel";
-            this.hoursLabel.Size = new System.Drawing.Size(187, 24);
+            this.hoursLabel.Size = new System.Drawing.Size(236, 29);
             this.hoursLabel.TabIndex = 8;
             this.hoursLabel.Text = "Enter Hours Worked:";
             // 
             // hoursInput
             // 
             this.hoursInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hoursInput.Location = new System.Drawing.Point(231, 100);
+            this.hoursInput.Location = new System.Drawing.Point(308, 123);
+            this.hoursInput.Margin = new System.Windows.Forms.Padding(4);
             this.hoursInput.Name = "hoursInput";
-            this.hoursInput.Size = new System.Drawing.Size(60, 29);
+            this.hoursInput.Size = new System.Drawing.Size(79, 34);
             this.hoursInput.TabIndex = 9;
             // 
             // num1Label
             // 
             this.num1Label.AutoSize = true;
             this.num1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num1Label.Location = new System.Drawing.Point(11, 166);
+            this.num1Label.Location = new System.Drawing.Point(15, 204);
+            this.num1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.num1Label.Name = "num1Label";
-            this.num1Label.Size = new System.Drawing.Size(71, 24);
+            this.num1Label.Size = new System.Drawing.Size(89, 29);
             this.num1Label.TabIndex = 10;
             this.num1Label.Text = "Num 1:";
             // 
             // num1Input
             // 
             this.num1Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num1Input.Location = new System.Drawing.Point(88, 161);
+            this.num1Input.Location = new System.Drawing.Point(117, 198);
+            this.num1Input.Margin = new System.Windows.Forms.Padding(4);
             this.num1Input.Name = "num1Input";
-            this.num1Input.Size = new System.Drawing.Size(60, 29);
+            this.num1Input.Size = new System.Drawing.Size(79, 34);
             this.num1Input.TabIndex = 11;
             // 
             // num2Input
             // 
             this.num2Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num2Input.Location = new System.Drawing.Point(231, 159);
+            this.num2Input.Location = new System.Drawing.Point(308, 196);
+            this.num2Input.Margin = new System.Windows.Forms.Padding(4);
             this.num2Input.Name = "num2Input";
-            this.num2Input.Size = new System.Drawing.Size(60, 29);
+            this.num2Input.Size = new System.Drawing.Size(79, 34);
             this.num2Input.TabIndex = 13;
             // 
             // num2Label
             // 
             this.num2Label.AutoSize = true;
             this.num2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num2Label.Location = new System.Drawing.Point(154, 164);
+            this.num2Label.Location = new System.Drawing.Point(205, 202);
+            this.num2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.num2Label.Name = "num2Label";
-            this.num2Label.Size = new System.Drawing.Size(71, 24);
+            this.num2Label.Size = new System.Drawing.Size(89, 29);
             this.num2Label.TabIndex = 12;
             this.num2Label.Text = "Num 2:";
             // 
             // maxButton
             // 
             this.maxButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maxButton.Location = new System.Drawing.Point(310, 160);
+            this.maxButton.Location = new System.Drawing.Point(413, 197);
+            this.maxButton.Margin = new System.Windows.Forms.Padding(4);
             this.maxButton.Name = "maxButton";
-            this.maxButton.Size = new System.Drawing.Size(88, 29);
+            this.maxButton.Size = new System.Drawing.Size(117, 36);
             this.maxButton.TabIndex = 14;
             this.maxButton.Text = "MAX";
             this.maxButton.UseVisualStyleBackColor = true;
@@ -179,18 +196,20 @@ namespace Decisions
             // 
             this.maxOutput.BackColor = System.Drawing.Color.White;
             this.maxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maxOutput.Location = new System.Drawing.Point(413, 159);
+            this.maxOutput.Location = new System.Drawing.Point(551, 196);
+            this.maxOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.maxOutput.Name = "maxOutput";
-            this.maxOutput.Size = new System.Drawing.Size(388, 29);
+            this.maxOutput.Size = new System.Drawing.Size(517, 36);
             this.maxOutput.TabIndex = 15;
             this.maxOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // yearButton
             // 
             this.yearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yearButton.Location = new System.Drawing.Point(310, 223);
+            this.yearButton.Location = new System.Drawing.Point(413, 274);
+            this.yearButton.Margin = new System.Windows.Forms.Padding(4);
             this.yearButton.Name = "yearButton";
-            this.yearButton.Size = new System.Drawing.Size(88, 29);
+            this.yearButton.Size = new System.Drawing.Size(117, 36);
             this.yearButton.TabIndex = 16;
             this.yearButton.Text = "YEAR";
             this.yearButton.UseVisualStyleBackColor = true;
@@ -200,35 +219,82 @@ namespace Decisions
             // 
             this.yearOutput.BackColor = System.Drawing.Color.White;
             this.yearOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yearOutput.Location = new System.Drawing.Point(413, 223);
+            this.yearOutput.Location = new System.Drawing.Point(551, 274);
+            this.yearOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.yearOutput.Name = "yearOutput";
-            this.yearOutput.Size = new System.Drawing.Size(388, 29);
+            this.yearOutput.Size = new System.Drawing.Size(517, 36);
             this.yearOutput.TabIndex = 17;
             this.yearOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // yearInput
             // 
             this.yearInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yearInput.Location = new System.Drawing.Point(231, 223);
+            this.yearInput.Location = new System.Drawing.Point(308, 274);
+            this.yearInput.Margin = new System.Windows.Forms.Padding(4);
             this.yearInput.Name = "yearInput";
-            this.yearInput.Size = new System.Drawing.Size(60, 29);
+            this.yearInput.Size = new System.Drawing.Size(79, 34);
             this.yearInput.TabIndex = 18;
+            this.yearInput.TextChanged += new System.EventHandler(this.yearInput_TextChanged);
             // 
             // yearLabel
             // 
             this.yearLabel.AutoSize = true;
             this.yearLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yearLabel.Location = new System.Drawing.Point(106, 226);
+            this.yearLabel.Location = new System.Drawing.Point(141, 278);
+            this.yearLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.yearLabel.Name = "yearLabel";
-            this.yearLabel.Size = new System.Drawing.Size(119, 24);
+            this.yearLabel.Size = new System.Drawing.Size(152, 29);
             this.yearLabel.TabIndex = 19;
             this.yearLabel.Text = "Enter a Year:";
             // 
+            // integerlable
+            // 
+            this.integerlable.BackColor = System.Drawing.Color.Transparent;
+            this.integerlable.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.integerlable.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.integerlable.Location = new System.Drawing.Point(97, 346);
+            this.integerlable.Name = "integerlable";
+            this.integerlable.Size = new System.Drawing.Size(190, 33);
+            this.integerlable.TabIndex = 20;
+            this.integerlable.Text = "Enter an Integer:";
+            this.integerlable.Click += new System.EventHandler(this.integerlable_Click);
+            // 
+            // integerinput
+            // 
+            this.integerinput.Location = new System.Drawing.Point(308, 348);
+            this.integerinput.Multiline = true;
+            this.integerinput.Name = "integerinput";
+            this.integerinput.Size = new System.Drawing.Size(79, 33);
+            this.integerinput.TabIndex = 21;
+            // 
+            // Entebutton
+            // 
+            this.Entebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Entebutton.Location = new System.Drawing.Point(413, 348);
+            this.Entebutton.Name = "Entebutton";
+            this.Entebutton.Size = new System.Drawing.Size(117, 33);
+            this.Entebutton.TabIndex = 22;
+            this.Entebutton.Text = "ENTER";
+            this.Entebutton.UseVisualStyleBackColor = true;
+            this.Entebutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // integeroutput
+            // 
+            this.integeroutput.BackColor = System.Drawing.Color.White;
+            this.integeroutput.Location = new System.Drawing.Point(556, 348);
+            this.integeroutput.Name = "integeroutput";
+            this.integeroutput.Size = new System.Drawing.Size(512, 82);
+            this.integeroutput.TabIndex = 23;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 299);
+            this.ClientSize = new System.Drawing.Size(1109, 439);
+            this.Controls.Add(this.integeroutput);
+            this.Controls.Add(this.Entebutton);
+            this.Controls.Add(this.integerinput);
+            this.Controls.Add(this.integerlable);
             this.Controls.Add(this.yearLabel);
             this.Controls.Add(this.yearInput);
             this.Controls.Add(this.yearOutput);
@@ -248,9 +314,11 @@ namespace Decisions
             this.Controls.Add(this.voteOutput);
             this.Controls.Add(this.voteButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Decisions";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,6 +343,10 @@ namespace Decisions
         private System.Windows.Forms.Label yearOutput;
         private System.Windows.Forms.TextBox yearInput;
         private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.Label integerlable;
+        private System.Windows.Forms.TextBox integerinput;
+        private System.Windows.Forms.Button Entebutton;
+        private System.Windows.Forms.Label integeroutput;
     }
 }
 
--- Decisions/Form1.cs
+++ Decisions/Form1.cs
@@ -23,8 +23,20 @@ namespace Decisions
             /// Create a variable to hold a person's age. Then use 
             /// that variable to determine if the person is old 
             /// enough to vote. Display an appropriate message based
-            /// on whether they can or can't vote            
-           
+            /// on whether they can or can't vote  
+
+            int age;
+            age = Convert.ToInt32(ageInput.Text);
+
+            if (age < 18)
+            {
+                voteOutput.Text = "you can't vote";
+
+            }
+            else
+            {
+                voteOutput.Text = "you can vote";
+            }
 
         }
 
@@ -41,7 +53,23 @@ namespace Decisions
             /// Sample output for 45 hours:
             /// You worked 40 hours plus 5 hours overtime
 
-      
+            int hours, overtime;
+
+            hours =  Convert.ToInt32(hoursInput.Text);
+
+            if (hours > 40)
+            {
+                overtime = hours - 40;
+                workOutput.Text = $"you worked 40 hourse plus {overtime} hour(s) overtime";
+
+            }
+
+            else
+            {
+                workOutput.Text = $"you worked {hours} hours";
+
+            }
+
 
         }
 
@@ -56,7 +84,22 @@ namespace Decisions
             /// 
             /// Sample output for inputs 22, 77
             /// The max value is: 77
-            
+
+            int num1, num2;
+            num1 = Convert.ToInt32(num1Input.Text);
+            num2 = Convert.ToInt32(num2Input.Text);
+
+            if (num1 > num2)
+            {
+                maxOutput.Text = $"The max value is {num1}";
+            }
+
+            else
+            {
+                maxOutput.Text = $"The max value is {num2}";
+
+            }
+
 
         }
 
@@ -70,7 +113,61 @@ namespace Decisions
             /// 
             /// Sample output for inputs 2016
             /// 2016 is a leap year
+           int year = Convert.ToInt32(yearInput.Text);
+
+            if (year % 4 == 0)
+            {
+                yearOutput.Text = $"{year} is a leap year";
+            }
+
+            else
+            {
+                yearOutput.Text = $"{year} is not a leap year";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yearInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //takes an integer as input
+            // if statements to test if it is positive or negative 
+            //check to see if the entered value is divisible by 7
            
+
+            int integer = Convert.ToInt32(integerinput.Text);
+
+            if (integer >= 0)
+            {
+                integeroutput.Text = $"{integer} is a positive number";
+
+            }
+
+            else
+            {
+                integeroutput.Text = $"{integer} is a negative number";
+            }
+
+            if (integer % 7 == 0)
+            {
+                integeroutput.Text += $"{integer} is divisable by 7";
+            }
+            else
+            {
+                integeroutput.Text += $"{integer} is not divisable by 7";
+            }
+        }
+
+        private void integerlable_Click(object sender, EventArgs e)
+        {
 
         }
     }
